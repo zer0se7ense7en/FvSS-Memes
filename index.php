10826ce76e6cc53edd334ef8c6a85256109e3d85
zer0se7ense7en <chrisrtianlange@gmail.com>
2020-10-18 17:38:52 +0200

    Added a title area for the memes

--- index.php
+++ index.php
@@ -14,10 +14,9 @@
   <body>
     <div class="container-fluid bg-dark">
       <div class="container-sm bg-dark p-2">
-        <?php
-        $txt = "HelloWorld!";
-        echo "<h2>" . $txt . "</h2>";
-        ?>
+
+        <?php $txt = "HelloWorld!";echo "<h2>" . $txt . "</h2>"; ?>
+
         <div class="row m-2"> <!-- Reihe 1 Überschrift-->
           <div class="col"> <!-- Spalte 1.1 -->
             <h1 class="text-center text-white" style="font-family: Copperplate; margin-top: 1rem; letter-spacing: .5rem">SteinSchule.Memes</h1>
@@ -68,21 +67,119 @@
 
         <div class="row m-1 mt-3"> <!-- Reihe 3 -->
   <!-- Spalte 3.1 "Memes"-->
-          <div class="col-sm-8 p-2 border">
-            <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
+          <div class="col-sm-8 p-2">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
               <div class="carousel-inner">
-                <div class="carousel-item active" data-interval="1">
-                  <div class="card " >
-                    <img src="https://i.redd.it/qsldnfyg7he41.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                <div class="carousel-item active">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">Metric is Superior</h5>
                     </div>
+                    <img src="https://i.redd.it/qsldnfyg7he41.png" class="card-img" alt="...">
                   </div>
                 </div>
-                <div class="carousel-item" data-interval="100000000">
-                  <img src="https://i.redd.it/aeq5km6zd2q51.jpg" class="d-block w-100" alt="...">
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/5e0hp1cmezh51.jpg" class="card-img" alt="...">
+                  </div>
                 </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/d6kl4ub0kbj51.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/7if9znvrx4e51.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/y0meeesjahl51.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/i4el1q6ql7841.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/c8bte6xhlip31.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/ava1gf9gb0851.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/w7zqpp34zpi51.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/8uf6g88z7id41.png" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/fe69axdca9h31.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+                <div class="carousel-item">
+                  <div class="card-body" >
+                    <div class="card-header bg-secondary">
+                      <h5 class="card-title">A Good Title</h5>
+                    </div>
+                    <img src="https://i.redd.it/1pjz301eb0v41.jpg" class="card-img" alt="...">
+                  </div>
+                </div>
+
+
               </div>
               <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -93,7 +190,7 @@
                 <span class="sr-only">Next</span>
               </a>
             </div>
-            <!-- Spicy Meme Sources: https://i.redd.it/hygjwamhubh51.jpg https://i.redd.it/1q6m5gynk4u41.jpg https://i.redd.it/y0meeesjahl51.jpg https://i.redd.it/7if9znvrx4e51.jpg https://i.redd.it/d6kl4ub0kbj51.jpg https://i.redd.it/5e0hp1cmezh51.jpg https://i.redd.it/cfk0sl4p9sy31.jpg https://i.redd.it/tmiuyhl58rd51.jpg -->
+            <!-- Spicy Meme Sources: https://i.redd.it/cfk0sl4p9sy31.jpg https://i.redd.it/tmiuyhl58rd51.jpg -->
           </div>
 
           <div class="col-sm-4 p-2"> <!-- Spalte 3.2 -->
